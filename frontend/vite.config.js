fa1a426424812b64a047070d0ca5eed70b4b0826
Joshua Chuah <xjoshua96@gmail.com>
2024-11-06 14:30:33 -0500

    vite config fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,25 +2,29 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  publicDir: 'public',
-  root: path.resolve(__dirname),
-  define: {
-    'process.env.VITE_CLOUDINARY_CLOUD_NAME': JSON.stringify(env.VITE_CLOUDINARY_CLOUD_NAME)
-  },
-  envDir: './',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html')
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    publicDir: 'public',
+    root: path.resolve(__dirname),
+    define: {
+      'process.env.VITE_CLOUDINARY_CLOUD_NAME': JSON.stringify(env.VITE_CLOUDINARY_CLOUD_NAME)
+    },
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+      assetsDir: 'assets',
+      rollupOptions: {
+        input: path.resolve(__dirname, 'index.html')
+      }
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
     }
   }
 })
