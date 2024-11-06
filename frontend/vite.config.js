ff4668b37055b030f06a5cd485be546634fed219
Joshua Chuah <xjoshua96@gmail.com>
2024-11-06 08:32:10 -0500

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
+  base: '/',
   assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.jpeg'],
   build: {
     assetsDir: 'assets'
