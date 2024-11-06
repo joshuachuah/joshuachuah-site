a6f80b0082da30bcd434d638eb0c80b4f8748c78
Joshua Chuah <xjoshua96@gmail.com>
2024-11-06 09:11:31 -0500

    updated vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
+  assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.jpeg'],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
-  },
-  root: path.resolve(__dirname, ''),
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
-  },
-  base: '/',
-  assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.jpeg'],
-  build: {
-    assetsDir: 'assets'
   }
 })
