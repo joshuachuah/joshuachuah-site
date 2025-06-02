e31ed808628c8b875f84e904baf64a36a33ee99b
Joshua Chuah <xjoshua96@gmail.com>
2025-06-02 08:51:20 -0400

    removed unsused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import './styles/global.css';
-import './styles/Navbar.css';
-import './styles/Hero.css';
-import './styles/About.css';
-import './styles/Skills.css';
-import './styles/Projects.css';
-import './styles/Contact.css';
 
 const App = () => {
   return (
