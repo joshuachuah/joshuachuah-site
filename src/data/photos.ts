70b33f4ddbe66cf0bd84ff42458da9623768b5ed
Joshua Chuah <xjoshua96@gmail.com>
2025-06-08 20:53:42 -0400

    photos.ts

---
+++ src/data/photos.ts
@@ -0,0 +1,26 @@
+export const photos = [
+  {
+    image: "/images/1.jpeg",
+    date: "December 2023",
+    location: "Liverpool, UK",
+    caption: "Visited my boyhood football club :)",
+  },
+  {
+    image: "/images/2.jpeg",
+    date: "December 2023",
+    location: "Picacho Peak, AZ",
+    caption: "I struggled to get up there",
+  },
+  {
+    image: "/images/3.jpeg",
+    date: "July 2023",
+    location: "Everland, South Korea",
+    caption: "YOLO-ed that scary roller coaster",
+  },
+  {
+    image: "/images/4.jpeg",
+    date: "December 2021",
+    location: "Top of The Rock, NY",
+    caption: "Covid was not fun",
+  }
+];
