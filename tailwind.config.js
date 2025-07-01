46788bff4091a90b225dc557142dadcf8730bd84
Joshua Chuah <xjoshua96@gmail.com>
2025-07-01 15:57:09 -0400

    additional bg colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -91,6 +91,11 @@ module.exports = {
         'bg-orange-500',
         'bg-yellow-500',
         'bg-indigo-500',
+        'bg-blue-500',
+        'bg-teal-500',
+        'bg-green-500',
+
+        'gray-400'
       ]
     },
   },
